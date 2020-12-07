ec6ad01d83b5d08015ff1538c30dc4a0fb4234db
unknown <maticnarex@gmail.com>
2020-12-07 16:54:17 +0100

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- Site/script.js
+++ Site/script.js
@@ -1,9 +1,25 @@
-function getcube(){  
-    var password=document.getElementById("number").value;  
+function getText(){  
+    var password=document.getElementById("number").value; 
+    var hiddenMessage=document.getElementById("hiddenMessage"); 
+    var hiddenMessage2=document.getElementById("hiddenMessage2"); 
+
+
     if (password === "test") {
         alert("Correct");  
+        hiddenMessage.style.visibility= "visible";
+        waited();
     }
+
     else {
-        alert("Wrong password. Try again.")
+            alert("Wrong password. Try again.")
+        
     }
 }  
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+
+const waited = async () => {
+  await delay(5000);
+  hiddenMessage2.style.visibility= "visible";
+};
